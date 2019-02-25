aa27928bf541d62bef69b05110a7ee4f8edbaa43
Jake Gough <jgough@tesla.com>
2019-02-24 21:54:49 -0700

    added nuget

--- vars/nuget.groovy
+++ vars/nuget.groovy
@@ -2,11 +2,9 @@ import groovy.transform.Field
 
 @Field nugetDockerImage="microsoft/dotnet:2.0-sdk";
 
-def pushNugetPackage(nupkgDir, credentialsId, sourceUrl = null) {    
-    sourceArg = '';
-    if (sourceUrl != null) {
-        sourceArg = "--source '$sourceUrl'";
-    }
+def pushNugetPackage(nupkgDir, credentialsId, sourceUrl = null) {
+
+    sourceUrlOrDefault = sourceUrl ?: "https://www.nuget.org/api/v2/package";
 
     withCredentials([string(credentialsId: credentialsId, variable: "nuget_api_key")]) {
         docker.image(nugetDockerImage).inside("-u root") {
@@ -19,7 +17,7 @@ def pushNugetPackage(nupkgDir, credentialsId, sourceUrl = null) {
             def nupkgFile = sh(returnStdout: true, script: "ls -1 $nupkgDir/*.nupkg | grep -v symbols").toString().trim();
 
             sh """
-                dotnet nuget push '$nupkgFile' --api-key '$nuget_api_key' $sourceArg
+                dotnet nuget push '$nupkgFile' --source '$sourceUrlOrDefault' --api-key '$nuget_api_key'
             """
         }
     }
