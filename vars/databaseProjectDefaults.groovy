59a98cfac1d9ef99dcf7af478eff5c4b3cb013e7
jakegough <jakegough@gmail.com>
2025-04-08 13:48:32 -0600

    databaseProjectDefaults

---
+++ vars/databaseProjectDefaults.groovy
@@ -0,0 +1,8 @@
+
+def load(gitHubRepository) {
+    helper.gitHubUsername = 'jakegough-jaytwo'
+    helper.gitHubRepository = gitHubRepository
+    helper.gitHubTokenCredentialsId = 'github-jakegough-jaytwo-token'
+    helper.ejsonCredentialsId = 'ejson-f301f9'
+    helper.ejsonVariable = 'ejson_f301f9'
+}
