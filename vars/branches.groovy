db542da13d211485132bf1a0dfbffb989132a2fc
jakegough <jakegough@gmail.com>
2025-04-08 12:35:21 -0600

    branch helpers

--- vars/branches.groovy
+++ vars/branches.groovy
@@ -1,18 +1,18 @@
-isMasterBranch() {
+def isMasterBranch() {
     return env.BRANCH_NAME == 'master' \
         || env.BRANCH_NAME == 'main'
 }
 
-isDevelopBranch() {
+def isDevelopBranch() {
     return env.BRANCH_NAME == 'develop'
 }
 
-isReleaseBranch() {
+def isReleaseBranch() {
     env.BRANCH_NAME == 'release' \
         || env.BRANCH_NAME.startsWith('release/')
 }
 
-isDeploymentBranch() {
+def isDeploymentBranch() {
     return isMasterBranch() \
         || isDevelopBranch() \
         || isReleaseBranch()
