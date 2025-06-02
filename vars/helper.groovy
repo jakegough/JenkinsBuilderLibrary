96d08abc5395b4baa379a1096c48bd2f84171caf
jakegough <jakegough@gmail.com>
2025-06-02 09:55:01 -0600

    removing dead wood

--- vars/helper.groovy
+++ vars/helper.groovy
@@ -6,18 +6,11 @@ import groovy.transform.Field
 @Field bitbucketUsername = null;
 @Field bitbucketRepository = null;
 @Field bitbucketUserPassCredentialsId = "missing_bitbucketUserPassCredentialsId";
-@Field dockerRegistryCredentialsId = "missing_dockerRegistryCredentialsId";
-@Field dockerRegistry = null;
-@Field dockerImageName = null;
-@Field kubectlKubeConfigFileCredentialsId = "missing_kubectlKubeConfigFileCredentialsId";
-@Field kubectlVersion = null;
 @Field cleanWsExcludePattern = null;
 @Field xunitTestResultsPattern = null;
 @Field coberturaCoverageReport = null;
 @Field htmlCoverageReportDir = null;
 @Field htmlCoverageReportIndexFile = null;
-@Field ejsonCredentialsId = "missing_ejsonCredentialsId";
-@Field ejsonVariable = "missing_ejsonVariable";
 
 
 def run(nodeLabel, callback) {
@@ -116,124 +109,6 @@ def run(nodeLabel, callback) {
   }
 }
 
-def loadDbProjectDefaults(gitHubRepository) {
-    gitHubUsername = 'jakegough-jaytwo'
-    gitHubRepository = gitHubRepository
-    gitHubTokenCredentialsId = 'github-jakegough-jaytwo-token'
-    ejsonCredentialsId = 'ejson-f301f9'
-    ejsonVariable = 'ejson_f301f9'
-}
-
-def runDatabase() {
-    /*
-    * requires make targets:
-    * - clean
-    * - build
-    * - test
-    * - migrate-dev
-    * - migrate-prod
-    *
-    * also assumes the ejsonVariable is consistent with the secret configured in the docker-compose file
-    */
-
-    run('linux && make && docker', {
-        try {
-            stage ('Build') {
-                sh "make clean build"
-            }
-            stage ('Test Migration') {
-                sh "make test"
-            }
-            if (branches.isDeploymentBranch()){
-                withCredentials([string(credentialsId: ejsonCredentialsId, variable: ejsonVariable)]) {
-                    if (branches.isDevelopBranch()){
-                        stage ('Migrate Dev') {
-                            sh "make migrate-dev"
-                        }
-                    }
-                    if (branches.isMasterBranch()){
-                        stage ('Migrate Prod') {
-                            sh "make migrate-prod"
-                        }
-                    }
-                }
-            }
-        }
-        finally {
-            sh "make clean"
-        }
-    })
-}
-
-def loadNuGetProjectDefaults(gitHubRepository) {
-    gitHubUsername = 'jakegough-jaytwo'
-    gitHubRepository = gitHubRepository
-    gitHubTokenCredentialsId = 'github-jakegough-jaytwo-token'
-    xunitTestResultsPattern = 'out/testResults/**/*.trx'
-    coberturaCoverageReport = 'out/coverage/Cobertura.xml';
-    htmlCoverageReportDir = 'out/coverage/html';
-}
-
-def runNuGetProject() {
-    /*
-    * requires make targets:
-    * - docker-builder
-    * - unit-test
-    * - pack
-    * - pack-beta
-    * - nuget-check
-    * - nuget-push
-    * - docker-copy-from-builder-output
-    * - docker-clean
-    *
-    * also assumes the ejsonVariable is consistent with the secret configured in the docker-compose file
-    */
-
-    def nuGetCredentialsId = 'nuget-org-jaytwo'
-
-    run('linux && make && docker', {
-        def timestamp = getTimestamp()
-        def safeJobName = getSafeJobName()
-        def dockerLocalTag = "jenkins__${safeJobName}__${timestamp}"
-        def dockerBuilderTag = dockerLocalTag + "__builder"
-
-        withEnv(["DOCKER_TAG=${dockerLocalTag}", "TIMESTAMP=${timestamp}"]) {
-            try {
-                stage ('Build') {
-                    sh "make docker-builder"
-                }
-                docker.image(dockerBuilderTag).inside() {
-                    stage ('Unit Test') {
-                        sh "make unit-test"
-                    }
-                    stage ('Pack') {
-                        if(branches.isMasterBranch()){
-                            sh "make pack"
-                        } else {
-                            sh "make pack-beta"
-                        }
-                    }
-                    stage ('NuGet Check Version') {
-                        sh "make nuget-check"
-                    }
-                    if (branches.isDeploymentBranch()) {
-                        withCredentials([string(credentialsId: nuGetCredentialsId, variable: "NUGET_API_KEY")]) {
-                            stage ('NuGet Push') {
-                                sh "make nuget-push"
-                            }
-                        }
-                    }
-                }
-            }
-            finally {
-                // inside the withEnv()
-                sh "make docker-copy-from-builder-output"
-                sh "make docker-clean"
-            }
-        }
-    })
-}
-
 def getTimestamp() {
     if (isUnix()) {
         return sh(returnStdout: true, script: "date +'%Y%m%d%H%M%S'").toString().trim()
@@ -298,61 +173,6 @@ def updateBuildStatusFailed(gitCommitHash = null) {
     {
         bitbucketHelper.updateBuildStatusFailed(bitbucketUserPassCredentialsId, bitbucketUsername, bitbucketRepository, gitCommitHash);
     }
-}
-
-def getDockerRegistryImageName(image = null, registry = null, credentialsId = null) {
-    def imageOrDefault = image ?: dockerImageName;
-    def registryOrDefault = registry ?: dockerRegistry;
-    def credentialsIdOrDefault = credentialsId ?: dockerRegistryCredentialsId;
-    return dockerHelper.getRegistryImageName(imageOrDefault, credentialsIdOrDefault, registryOrDefault)
-}
-
-def tagDockerImage(sourceImage, targetImage) {
-    dockerHelper.tag(sourceImage, targetImage);
-}
-
-def dockerLogin(registry = null, credentialsId = null) {
-    def registryOrDefault = registry ?: dockerRegistry;
-    def credentialsIdOrDefault = credentialsId ?: dockerRegistryCredentialsId;
-    dockerHelper.login(credentialsIdOrDefault, registryOrDefault);
-}
-
-def pushDockerImage(image) {
-    dockerHelper.pushImage(image);
-}
-
-def removeDockerImage(image) {
-    dockerHelper.removeImage(image);
-}
-
-def tagAndPushDockerImageBeta(dockerLocalTag, timestamp = null, image = null, credentialsId = null, registry = null) {
-    def timestampOrDefault = timestamp ?: getTimestamp();
-    def imageOrDefault = image ?: dockerImageName;
-    def registryOrDefault = registry ?: dockerRegistry;
-    def credentialsIdOrDefault = credentialsId ?: dockerRegistryCredentialsId;
-
-    dockerHelper.tagAndPushImageBeta(dockerLocalTag, timestampOrDefault, imageOrDefault, credentialsIdOrDefault, registryOrDefault)
-}
-
-def tagAndPushDockerImageRelease(dockerLocalTag, timestamp = null, image = null, credentialsId = null, registry = null) {
-    def timestampOrDefault = timestamp ?: getTimestamp();
-    def imageOrDefault = image ?: dockerImageName;
-    def registryOrDefault = registry ?: dockerRegistry;
-    def credentialsIdOrDefault = credentialsId ?: dockerRegistryCredentialsId;
-
-    dockerHelper.tagAndPushImageRelease(dockerLocalTag, timestampOrDefault, imageOrDefault, credentialsIdOrDefault, registryOrDefault)
-}
-
-def withKubectl(callback) {
-    kubernetesHelper.withKubectl(kubectlKubeConfigFileCredentialsId, kubectlVersion, callback);
-}
-
-def kubectlApply(path) {
-    kubernetesHelper.kubectlApply(kubectlKubeConfigFileCredentialsId, kubectlVersion, path);
-}
-
-def kubectlApplyDryRun(path) {
-    kubernetesHelper.kubectlApplyDryRun(kubectlKubeConfigFileCredentialsId, kubectlVersion, path);
 }
 
 def verifyPluginExists(pluginId) {
