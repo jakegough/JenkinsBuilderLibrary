3e0a061edde47e2a8b7d24fb377cd0bda1ea67b7
jakegough <jakegough@gmail.com>
2025-06-02 09:28:12 -0600

    updated withEjson

--- vars/withEjson.groovy
+++ vars/withEjson.groovy
@@ -1,11 +1,12 @@
 def call(String ejsonPublicKey, Closure callback) {
+    if (!ejsonPublicKey?.trim()) {
+        error("ejsonPublicKey must not be null or empty")
+    }
 
-    withCredentials([string(
-        credentialsId: "EJK_$ejsonPublicKey",
-        variable: 'ejson_private_key')]) {
+    echo "Loading EJSON private key into EJK_${ejsonPublicKey}"
 
-        def ejsonEnvVar = "EJK_${ejsonPublicKey}=${ejson_private_key}"
-        withEnv([ejsonEnvVar]) {
+    withCredentials([string(credentialsId: "EJK_$ejsonPublicKey", variable: 'ejson_private_key')]) {
+        withEnv(["EJK_${ejsonPublicKey}=${ejson_private_key}"]) {
             callback()
         }
     }
